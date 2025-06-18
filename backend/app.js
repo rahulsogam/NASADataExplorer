a71dc5067670546068332c052a1d15f056f26021
Rahulsogam <sogam.rahul188@gmail.com>
2025-06-18 13:08:35 +0100

    CORS

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,33 @@ const app = express();
 
 
 app.use(express.json());
+var corsOptions = {
+  methods: "GET, PUT, POST, DELETE, PATCH",
+  credentials: true,
+  origin: "https://nasa-explorer-j0vs.onrender.com",
+};
 
-app.use(cors({
-  origin: [
-    'https://nasa-data-explorer-nu.vercel.app',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+
+  next();
+};
+app.use(allowCrossDomain);
+app.options("*", cors());
+app.use(cors(corsOptions));
 
 // Routes
 app.get("/",(req,res)=>{
