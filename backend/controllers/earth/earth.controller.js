894e7dad10253067a163583a5fddf15a4a9e8c1a
Rahulsogam <sogam.rahul188@gmail.com>
2025-06-18 12:41:27 +0100

    CORS Policy

--- backend/controllers/earth/earth.controller.js
+++ backend/controllers/earth/earth.controller.js
@@ -5,6 +5,17 @@ exports.getEarthImage = async (req, res, next) => {
     const { lon, lat, date, dim } = req.query;
     const image = await getEarthImagery(lon, lat, date, dim);
     
+    const origin = req.headers.origin;
+    const allowedOrigins = [
+      'http://localhost:3000',
+      'https://nasa-data-explorer-nu.vercel.app'
+    ];
+
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+
     // Set proper content-type for image response
     res.set(200, {
       'Content-Type': 'image/png',
