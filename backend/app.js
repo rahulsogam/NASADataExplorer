4f38246c6ff0bab418543cd9d87b6d58f20b0561
Rahulsogam <sogam.rahul188@gmail.com>
2025-06-18 14:07:08 +0100

    Vercel

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://nasa-data-explorer-nu.vercel.app', 
+  origin: 'localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
