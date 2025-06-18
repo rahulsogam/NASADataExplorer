bd204a6875f983b48bcc1919b23c89c3fe5ce950
Rahulsogam <sogam.rahul188@gmail.com>
2025-06-18 12:48:13 +0100

    Vercel JSON

--- backend/controllers/earth/earth.controller.js
+++ backend/controllers/earth/earth.controller.js
@@ -5,26 +5,14 @@ exports.getEarthImage = async (req, res, next) => {
     const { lon, lat, date, dim } = req.query;
     const image = await getEarthImagery(lon, lat, date, dim);
     
-    const origin = req.headers.origin;
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'https://nasa-data-explorer-nu.vercel.app'
-    ];
-
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-
     // Set proper content-type for image response
-    res.set(200, {
+    res.writeHead(200, {
       'Content-Type': 'image/png',
       'Content-Length': image.length,
       'Content-Disposition': 'inline'
     });
-    
-    res.send(image, 'binary')
-    
+    res.end(image, 'binary')
+    // res.send(image);
   } catch (error) {
     next(error);
   }
