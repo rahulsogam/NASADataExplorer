0ccbcc4874d7cb14dc83d5c50bc71bc17d095016
Rahulsogam <sogam.rahul188@gmail.com>
2025-06-18 14:11:45 +0100

    Vercel

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'localhost:3000', 
+  origin: 'http://localhost:3000', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
